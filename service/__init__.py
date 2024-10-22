4e9c0931d570da5c5d82ef8f487abbe1b22bc101
Samgaikw <sampadagaikwad2000@gmail.com>
2024-10-22 14:56:38 +0530

    Update __init__.py

--- service/__init__.py
+++ service/__init__.py
@@ -10,7 +10,6 @@ from service.common import log_handlers
 
 log_handlers.init_logging(app, "gunicorn.error")
 
-
 app.logger.info(70 * "*")
 app.logger.info("  S E R V I C E   R U N N I N G  ".center(70, "*"))
 app.logger.info(70 * "*")
